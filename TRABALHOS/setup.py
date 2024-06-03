2c490bef05280259f7ddd93717962225a2178da4
GIOVANI SIQUEIRA DA SILVA <1136492@ATITUS.LOCAL>
2024-06-03 20:47:14 -0300

    primeira vez

---
+++ TRABALHOS/setup.py
@@ -0,0 +1,14 @@
+# pip install cx_freeze
+import cx_Freeze
+executaveis = [ 
+                cx_Freeze.Executable(script="main.py")]
+cx_Freeze.setup(
+    name = "Iron Man",
+    options={
+        "build_exe":{
+            "packages":["pygame"],
+            "include_files":["assets"]
+        }
+    }, executables = executaveis
+)
+    
